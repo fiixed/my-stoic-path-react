57b91f879c9fbe313faa6da4122a6828cc37dca8
Andrew Bell <fiixed@gmail.com>
2023-08-31 09:58:02 -0500

    basic journal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container">
+      <h1>Journal</h1>
+      <div>
+        <label className="label">TimeStamp</label>
+      </div>
+      <div>
+        <textarea placeholder="Entry" className="textarea"></textarea>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOM.createRoot(rootEl);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
