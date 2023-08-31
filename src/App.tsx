d07e2cc271fdb841954b35d4921afdf4fcf5485e
Andrew Bell <fiixed@gmail.com>
2023-08-31 11:40:00 -0500

    appbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import AppButton from './components/AppButton';
+import JournalEntry from './components/JournalEntry';
+
 const App = () => {
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className="space-y-6 bg-white shadow-md rounded p-5">
         <h1 className="font-semibold text-2xl text-center">Journal</h1>
-        
         <div>
           <textarea
             placeholder="Entry"
@@ -11,43 +13,16 @@ const App = () => {
           ></textarea>
         </div>
         <div className="text-right">
-          <button className="bg-blue-500 text-white px-5 py-2 rounded">
-            Submit
-          </button>
+          <AppButton
+            title="Submit"
+            type="regular"
+            onClick={() => console.log('Submit clicked')}
+          />
         </div>
       </div>
 
-      {/* Note Items */}
-      <div className="bg-white shadow-md rounded p-5">
-        <p className="font-semibold mb-4 text-gray-700 text-lg">
-          Lorem ipsum dolor sit amet.
-        </p>
-        <div className="space-x-4">
-          <button className="bg-blue-500 text-white p-2 rounded">View</button>
-          <button className="bg-gray-700 text-white p-2 rounded">Edit</button>
-          <button className="bg-red-500 text-white p-2 rounded">Delete</button>
-        </div>
-      </div>
-      <div className="bg-white shadow-md rounded p-5">
-        <p className="font-semibold mb-4 text-gray-700 text-lg">
-          Lorem ipsum dolor sit amet.
-        </p>
-        <div className="space-x-4">
-          <button className="bg-blue-500 text-white p-2 rounded">View</button>
-          <button className="bg-gray-700 text-white p-2 rounded">Edit</button>
-          <button className="bg-red-500 text-white p-2 rounded">Delete</button>
-        </div>
-      </div>
-      <div className="bg-white shadow-md rounded p-5">
-        <p className="font-semibold mb-4 text-gray-700 text-lg">
-          Lorem ipsum dolor sit amet.
-        </p>
-        <div className="space-x-4">
-          <button className="bg-blue-500 text-white p-2 rounded">View</button>
-          <button className="bg-gray-700 text-white p-2 rounded">Edit</button>
-          <button className="bg-red-500 text-white p-2 rounded">Delete</button>
-        </div>
-      </div>
+      {/* Journal Entry Items */}
+      <JournalEntry timestamp="test" />
     </div>
   );
 };
---
+++ src/components/JournalEntry.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import AppButton from './AppButton';
+
+interface Props {
+  timestamp: string;
+}
+
+const JournalEntry: FC<Props> = ({ timestamp }) => {
+  return (
+    <div className="bg-white shadow-md rounded p-5">
+      <p className="font-semibold mb-4 text-gray-700 text-lg">{timestamp}</p>
+      <div className="space-x-4">
+        <AppButton title="View" type="regular" />
+        <AppButton title="Edit" type="normal" />
+        <AppButton title="Delete" type="danger" />
+      </div>
+    </div>
+  );
+};
+
+export default JournalEntry;
