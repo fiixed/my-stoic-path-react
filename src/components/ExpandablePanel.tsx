a115a59347df660022c738b493658091117a7246
Andrew Bell <fiixed@gmail.com>
2023-09-06 16:02:39 -0500

    expandable panel works

---
+++ src/components/ExpandablePanel.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+function ExpandablePanel({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <div className="mb-2 border rounded">
+      <div
+        onClick={handleClick}
+        className="flex p-2 justify-between items-center cursor-pointer"
+      >
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
+        <div>{expanded ? <GoChevronDown /> : <GoChevronLeft />}</div>
+      </div>
+      {expanded && <div className="p-2 border-t text-gray-500">{children}</div>}
+    </div>
+  );
+}
+
+export default ExpandablePanel;
--- src/components/JournalListItem.tsx
+++ src/components/JournalListItem.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import moment from 'moment';
-import AppButton from './AppButton';
+import { GoTrash } from 'react-icons/go';
+import { GrEdit } from 'react-icons/gr';
+import ExpandablePanel from './ExpandablePanel';
 
 interface Props {
   timestamp: Date;
@@ -9,21 +11,40 @@ interface Props {
   onDeleteClick?(): void;
 }
 
-const JournalListItem: FC<Props> = ({ timestamp, description, onEditClick, onDeleteClick }) => {
+const JournalListItem: FC<Props> = ({
+  timestamp,
+  description,
+  onEditClick,
+  onDeleteClick,
+}) => {
   const formattedTimestamp = moment(timestamp).format('MM-DD-YYYY  hh:mm:ss a');
 
+  const header = (
+    <>
+      <button onClick={onDeleteClick}>
+        <GoTrash />
+      </button>
+      <button onClick={onEditClick}>
+        <GrEdit />
+      </button>
+      {/* {error && <div>Error deleting user.</div>} */}
+      {formattedTimestamp}
+    </>
+  );
+
   return (
-    <div className="bg-white shadow-md rounded p-5">
-      <p className="font-semibold mb-4 text-gray-700 text-lg">
-        {formattedTimestamp}
-      </p>
-      <p className=" mb-4 text-gray-500 text-lg">{description}</p>
-      <div className="space-x-4">
-        <AppButton title="View" type="regular" />
-        <AppButton onClick={onEditClick} title="Edit" type="normal" />
-        <AppButton onClick={onDeleteClick} title="Delete" type="danger" />
-      </div>
-    </div>
+    // <div className="bg-white shadow-md rounded p-5">
+    //   <p className="font-semibold mb-4 text-gray-700 text-lg">
+    //     {formattedTimestamp}
+    //   </p>
+    //   <p className=" mb-4 text-gray-500 text-lg">{description}</p>
+    //   <div className="space-x-4">
+
+    //     <AppButton onClick={onEditClick} title="Edit" type="normal" />
+    //     <AppButton onClick={onDeleteClick} title="Delete" type="danger" />
+    //   </div>
+    // </div>
+    <ExpandablePanel header={header}>{description}</ExpandablePanel>
   );
 };
 
